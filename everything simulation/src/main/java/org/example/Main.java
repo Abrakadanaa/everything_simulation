a47a7a005a5967aca749af1530e885d714e86707
Laurin <laurin@erlacher.dev>
2026-09-25 17:47:57 +0200

    moving dot v1

--- everything simulation/src/main/java/org/example/Main.java
+++ everything simulation/src/main/java/org/example/Main.java
@@ -5,9 +5,18 @@ import codedraw.CodeDraw;
 public class Main {
     public static void main(String[] args) {
         System.out.println("git test");
+        int x = 0;
         try (CodeDraw cd = new CodeDraw(400, 400)) {
-            cd.fillCircle(300, 200, 50);
             cd.show();
+            while (!cd.isClosed()) {
+                cd.clear();
+                cd.drawPoint(x,4);
+                x++;
+                if (x > 400) {
+                    x = 0;
+                }
+                cd.show(8);
+            }
         }
     }
 }
