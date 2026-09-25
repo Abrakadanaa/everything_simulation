357b9f2f4a7f5f44402b49dd3267064d08893aaf
Laurin <laurin@erlacher.dev>
2026-09-25 18:07:31 +0200

    moving dot v2 alsocompile

--- everything simulation/src/main/java/org/example/Main.java
+++ everything simulation/src/main/java/org/example/Main.java
@@ -2,20 +2,25 @@ package org.example;
 
 import codedraw.CodeDraw;
 
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("git gut test");
         int x = 0;
+        int y = 0;
         try (CodeDraw cd = new CodeDraw(400, 400)) {
             cd.show();
+            cd.setColor(new Color(0,0,100));
+            cd.drawLine(10, 20, 20, 10);
             while (!cd.isClosed()) {
-                cd.clear();
-                cd.drawPoint(x,4);
+                cd.drawPoint(x,y);
                 x++;
                 if (x > 400) {
                     x = 0;
+                    y++;
                 }
-                cd.show(8);
+                cd.show();
             }
         }
     }
