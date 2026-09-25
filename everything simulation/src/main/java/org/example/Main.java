04481416ac54888b4fee1458cc3258a09f996187
Laurin <laurin@erlacher.dev>
2026-09-25 17:39:14 +0200

    maven setup v3

--- everything simulation/src/main/java/org/example/Main.java
+++ everything simulation/src/main/java/org/example/Main.java
@@ -1,7 +1,13 @@
 package org.example;
 
+import codedraw.CodeDraw;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("git test");
+        try (CodeDraw cd = new CodeDraw(400, 400)) {
+            cd.fillCircle(300, 200, 50);
+            cd.show();
+        }
     }
 }
